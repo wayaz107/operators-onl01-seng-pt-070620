23842c73cae26e33168651ab3697101ba45ccdf9
Warda Ayaz <wayaz107@gmail.com>
2020-07-09 21:45:58 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,6 @@
+
+speed = 0
+
 def unsafe?(speed)
 if speed < 40 || speed > 60
   "true"
@@ -7,6 +10,6 @@ end
 
 
 def not_safe?(speed)
-  speed <40||speed >60? "true" : "false"
+  speed <40 || speed >60? "true" : "false"
 end
 	
